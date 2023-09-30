f5ff15847d291cdf0d678f5e0f16ccac84bf9aeb
Joan Camps <kmps84@gmail.com>
2023-09-30 17:15:42 +0200

    Implement the Edit button

--- gui.py
+++ gui.py
@@ -7,26 +7,41 @@ sg.theme("DarkAmber")
 label = sg.Text("Type in a To-Do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_todos(), key="todos", enable_events=True, size=[45, 10])
+edit_button = sg.Button("Edit")
 
 # Creamos la ventana de la GUI y el título. En layout añadimos las instancias a mostrar en un list de list. Cada list
 # representa una fila
 window = sg.Window("My To-Do App",
-                   layout=[[label], [input_box, add_button]],
-                   font=("Helvetica", 20))
+                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   font=("Helvetica", 16))
 #window.read()  # muestra la interface con las instancias (buttons y demás). A partir de la siguiente línea va el código
 
 while True:
-    event, value = window.read()
-    print(event)
-    print(value)
+    event, values = window.read()
+    print(1, event)
+    print(2, values)
+    print(3, values["todos"][0])
     match event:
         case "Add":
             todos = functions.get_todos()
             print(todos)
-            new_todo = value["todo"] + "\n"
+            new_todo = values["todo"] + "\n"
             todos.append(new_todo)
             print(todos)
             functions.write_todos(todos)
+            window["todos"].update(values=todos)
+        case "Edit":
+            todo_to_edit = values["todos"][0]
+            new_todo = values["todo"] + "\n"
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window["todos"].update(values=todos)
+        case "todos":
+            window["todo"].update(value=values["todos"][0])
         case sg.WIN_CLOSED:
             break
 

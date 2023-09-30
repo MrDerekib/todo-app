890bc4d76b615d91e7cf24dcb70133eefc56a895
Joan Camps <kmps84@gmail.com>
2023-09-30 16:07:07 +0200

    Implement the Add button

--- gui.py
+++ gui.py
@@ -3,16 +3,31 @@ import PySimpleGUI as sg
 
 sg.theme("DarkAmber")
 
-#creamos las instancias que queremos en la GUI
+# creamos las instancias que queremos en la GUI
 label = sg.Text("Type in a To-Do")
-input_box = sg.InputText(tooltip="Enter todo")
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
 
-#creamos la ventan de la GUI y el título. En layout añadimos las instancias a mostrar en un list de list. Cada list
-#representa una fila
-window = sg.Window("My To-Do App", layout=[[label], [input_box, add_button]])
-window.read() # muestra la interface con las instancias (buttons y demás). A partir de la siguiente línea va el código
+# Creamos la ventana de la GUI y el título. En layout añadimos las instancias a mostrar en un list de list. Cada list
+# representa una fila
+window = sg.Window("My To-Do App",
+                   layout=[[label], [input_box, add_button]],
+                   font=("Helvetica", 20))
+#window.read()  # muestra la interface con las instancias (buttons y demás). A partir de la siguiente línea va el código
 
-print("Hello")
+while True:
+    event, value = window.read()
+    print(event)
+    print(value)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            print(todos)
+            new_todo = value["todo"] + "\n"
+            todos.append(new_todo)
+            print(todos)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
 
 window.close()

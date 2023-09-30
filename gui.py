0ed1ed2f79b10d3e62093e0e36fd63d161396c84
Joan Camps <kmps84@gmail.com>
2023-09-30 21:35:58 +0200

    Add clock, handle edit and complete error whe empty selection

--- gui.py
+++ gui.py
@@ -1,9 +1,10 @@
 import functions
 import PySimpleGUI as sg
-
+import time
 sg.theme("DarkAmber")
 
 # creamos las instancias que queremos en la GUI
+clock = sg.Text("", key="clock")
 label = sg.Text("Type in a To-Do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
@@ -15,7 +16,8 @@ exit_button = sg.Button("Exit")
 # Creamos la ventana de la GUI y el título. En layout añadimos las instancias a mostrar en un list de list. Cada list
 # representa una fila
 window = sg.Window("My To-Do App",
-                   layout=[[label],
+                   layout=[[clock],
+                           [label],
                            [input_box, add_button],
                            [list_box, edit_button, complete_button],
                            [exit_button]],
@@ -23,7 +25,9 @@ window = sg.Window("My To-Do App",
 #window.read()  # muestra la interface con las instancias (buttons y demás). A partir de la siguiente línea va el código
 
 while True:
-    event, values = window.read()
+
+    event, values = window.read(timeout=200)
+    window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
     # print(1, event)
     # print(2, values)
     # print(3, values["todos"])
@@ -36,22 +40,29 @@ while True:
             # print(todos)
             functions.write_todos(todos)
             window["todos"].update(values=todos)
-        case "Edit":
-            todo_to_edit = values["todos"][0]
-            new_todo = values["todo"] + "\n"
 
-            todos = functions.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            functions.write_todos(todos)
-            window["todos"].update(values=todos)
+        case "Edit":
+            try:
+                todo_to_edit = values["todos"][0]
+                new_todo = values["todo"] + "\n"
+
+                todos = functions.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                functions.write_todos(todos)
+                window["todos"].update(values=todos)
+            except IndexError:
+                sg.popup("Please select an item first", font=("Helvetica", 16))
         case "Complete":
-            todo_to_complete = values["todos"][0]
-            todos = functions.get_todos()
-            todos.remove(todo_to_complete)
-            functions.write_todos(todos)
-            window["todos"].update(values=todos)
-            window["todo"].update(value="")
+            try:
+                todo_to_complete = values["todos"][0]
+                todos = functions.get_todos()
+                todos.remove(todo_to_complete)
+                functions.write_todos(todos)
+                window["todos"].update(values=todos)
+                window["todo"].update(value="")
+            except IndexError:
+                sg.popup("Please select an item first", font=("Helvetica", 16))
         case "Exit":
             break
         case "todos": #copia el valor seleccionado del Listbox al Textbox

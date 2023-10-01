01ca077b674479e2df05102a1671673590fc009d
Joan Camps <kmps84@gmail.com>
2023-10-01 23:19:38 +0200

    Implement 'Complete todo' feature

--- web.py
+++ web.py
@@ -9,6 +9,7 @@ def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
     functions.write_todos(todos)
+    st.session_state["new_todo"] = ""  # Clear textbox after add to-do
 
 
 st.title("My Todo App")
@@ -16,11 +17,16 @@ st.subheader("This is my todo app")
 st.write("This app is to increase tour productivity")
 
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]  # Delete item from session_state
+        st.rerun()  # Restart the script to update the checkboxes
 
 
 st.text_input(label="", placeholder="Add new todo..",
               on_change=add_todo, key="new_todo")
 
-st.session_state
+st.session_state #show in webapp events and values
